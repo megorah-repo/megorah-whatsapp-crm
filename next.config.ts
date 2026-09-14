27f68dc8febc00fa21e16ebaf1e50744c5af7f08
megorah-repo <johnsonsabharwal97@gmail.com>
2026-09-14 19:07:32 +0530

    security: remove unsafe-eval from CSP

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const SECURITY_HEADERS = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline'",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: blob: https:",
       "media-src 'self' blob: https://*.supabase.co",
