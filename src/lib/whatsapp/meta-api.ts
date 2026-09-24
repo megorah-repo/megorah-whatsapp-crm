fe04933d5edab2a0495603e338a8c59f60b22dcc
megorah-repo <johnsonsabharwal97@gmail.com>
2026-09-24 08:58:57 +0530

    Make Meta Graph API version configurable

--- src/lib/whatsapp/meta-api.ts
+++ src/lib/whatsapp/meta-api.ts
@@ -9,7 +9,7 @@
  * instead of a runtime rejection from Meta.
  */
 
-const META_API_VERSION = 'v21.0'
+const META_API_VERSION = process.env.META_GRAPH_API_VERSION || 'v21.0'
 const META_API_BASE = `https://graph.facebook.com/${META_API_VERSION}`
 
 export interface MetaSendResult {
